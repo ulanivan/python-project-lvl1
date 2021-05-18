d90eb3cc33bb4f7d4a2e7aafdd02d0521e577cbb
Ivan Ulanovich <i.ulanovich@examus.net>
2021-05-18 12:01:40 +0500

    fix brain-games

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
+import prompt
 
 def main():
     print("Welcome to the Brain Games!")
+    user_name = prompt.string('May I have your name? ')
+    print("Hello, {}!".format(user_name))
 
 
 if __name__ == '__main__':
